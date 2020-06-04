32a07510fc9c416b7c149478d63164adf6b30349
Valentyn Zolotarov <valik.zolotariov@mohawkgp.com>
2020-06-04 09:18:54 +0300

    Added unity project

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    /// кількість зібраних кубів
+    private int count;
+    /// Початкова кількість кубів
+    private int countCoub;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0; /// по замовчуванням зібрано 0 кубів
+      
+        countCoub = GameObject.FindGameObjectsWithTag("cubes").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            rb.AddForce(movement * speed);
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "cubes")
+        {
+            Destroy(other.gameObject);
+            count++;
+        }
+    }
+}
---
+++ Assets/camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    private Vector3 offset;
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+    void Update()
+    {
+        //transform.Rotate(new Vector3(15, 35, 28) * Time.deltaTime);
+    }
+
+}
